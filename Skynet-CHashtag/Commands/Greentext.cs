4398fba7abdb1eab7b7ee91abd953cda0ba202aa
OneYellowLemon <66096725+OneYellowLemon@users.noreply.github.com>
2022-09-02 19:54:29 -0400

    Move commands to their own file

---
+++ Skynet-CHashtag/Commands/Greentext.cs
@@ -0,0 +1,20 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using OpenAI;
+
+namespace Skynet_CHashtag.Commands;
+
+public class Greentext : BaseCommandModule
+{
+    [Command("greentext")]
+    public static async Task GreentextCommand(CommandContext ctx, string prompt) {
+        var api = new OpenAIAPI(apiKeys:"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");
+        var request = new CompletionRequestBuilder()
+            .WithPrompt($"write me a 4chan greentext \n{prompt}")
+            .WithMaxTokens(200)
+            .Build();
+        
+        var result = await api.Completions.CreateCompletionAsync(request);
+        await ctx.RespondAsync($"{result}");
+    }
+}
