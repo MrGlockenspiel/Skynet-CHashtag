b431dd1b0e5854d65251edf701784b46a369bd1c
OneYellowLemon <66096725+OneYellowLemon@users.noreply.github.com>
2022-09-11 00:12:42 -0400

    Fix a crash with temperature

--- Skynet-CHashtag/Commands/Gpt3.cs
+++ Skynet-CHashtag/Commands/Gpt3.cs
@@ -9,6 +9,15 @@ public class Gpt3Command : ApplicationCommandModule {
     [SlashCommand("gpt3", "Generate a response with GPT3")]
     public async Task Gpt3SlashCommand(InteractionContext ctx, [Option("prompt", "Prompt for generation")] string prompt,  [Option("temperature", "Generation temperature (0-1, 0.8 is default)")] double temperature = 0.8) {
         await ctx.Interaction.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+        switch (temperature)
+        {
+            case < 0:
+                temperature = 0.0;
+                break;
+            case > 1:
+                temperature = 1.0;
+                break;
+        }
         var request = new CompletionRequestBuilder()
             .WithPrompt($"{prompt}")
             .WithMaxTokens(100)
--- Skynet-CHashtag/Commands/Greentext.cs
+++ Skynet-CHashtag/Commands/Greentext.cs
@@ -9,6 +9,15 @@ public class GreentextCommand : ApplicationCommandModule {
     [SlashCommand("greentext", "Generate an AI 4chan greentext")]
     public async Task GreentextSlashCommand(InteractionContext ctx, [Option("prompt", "Prompt for generation")] string prompt, [Option("temperature", "Generation temperature (0-1, 0.8 is default)")] double temperature = 0.8) {
         await ctx.Interaction.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+        switch (temperature)
+        {
+            case < 0:
+                temperature = 0.0;
+                break;
+            case > 1:
+                temperature = 1.0;
+                break;
+        }
         var request = new CompletionRequestBuilder()
             .WithPrompt($"write me a 4chan greentext \n{prompt}")
             .WithMaxTokens(200)
